699dbddba33bcc92b3a42ab547d0775e1bec643b
Gareth Jones <jones258@gmail.com>
2022-06-15 14:39:35 +1200

    feat: support parsing commits from `go.mod` files

--- pkg/lockfile/parse-go-lock.go
+++ pkg/lockfile/parse-go-lock.go
@@ -4,10 +4,23 @@ import (
 	"fmt"
 	"golang.org/x/mod/modfile"
 	"io/ioutil"
+	"regexp"
 	"strings"
 )
 
 const GoEcosystem Ecosystem = "Go"
+
+func parseGoModVersion(version string) (string, string, string) {
+	re := regexp.MustCompile(`^v([\d.]+(?:-[\w.]+)?)[-.](\d{14})-(\w{12})$`)
+
+	matched := re.FindStringSubmatch(version)
+
+	if matched == nil {
+		return version, "", ""
+	}
+
+	return matched[1], matched[2], matched[3]
+}
 
 func ParseGoLock(pathToLockfile string) ([]PackageDetails, error) {
 	lockfileContents, err := ioutil.ReadFile(pathToLockfile)
@@ -25,10 +38,13 @@ func ParseGoLock(pathToLockfile string) ([]PackageDetails, error) {
 	packages := make([]PackageDetails, 0, len(parsedLockfile.Require))
 
 	for _, require := range parsedLockfile.Require {
+		version, _, commit := parseGoModVersion(require.Mod.Version)
+
 		packages = append(packages, PackageDetails{
 			Name:      require.Mod.Path,
-			Version:   strings.TrimPrefix(require.Mod.Version, "v"),
+			Version:   strings.TrimPrefix(version, "v"),
 			Ecosystem: GoEcosystem,
+			Commit:    commit,
 		})
 	}
 
